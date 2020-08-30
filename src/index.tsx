c360bbd11801dfeee7c5a70523e2018d2ca9a34e
root <root@DESKTOP-0FVSKR4.localdomain>
2020-08-30 21:44:59 +0430

    add font-awesome to project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "font-awesome/css/font-awesome.css";
+import "font-awesome/fonts/FontAwesome.otf";
 
 ReactDOM.render(
   <React.StrictMode>
