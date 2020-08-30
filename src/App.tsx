7fb2d8683d994f736c2c58cce6349b59f0c712dd
root <root@DESKTOP-0FVSKR4.localdomain>
2020-08-30 21:44:14 +0430

    Add button default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.scss";
-import { Header } from "./Layouts/Landing/Header/Header";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Path } from "./Path";
 import { Body } from "./Layouts/Landing/Body/Body";
 import { LoginLayout } from "./Layouts/LoginLayout/LoginLayout";
 
