593d5083170765f48879f9cc0ecda1abf6465e9f
Mohammad Tagizade <azartarh.tabriz@gmail.com>
2020-10-16 17:14:21 +0330

    add helper general funtions

--- src/Helper.tsx
+++ src/Helper.tsx
@@ -1,3 +1,83 @@
+import Axios from "axios";
+import { toast } from "react-toastify";
+import { RenderMessage } from "./Localization/RenderMessage";
+import { IUserData } from "./Types";
+
 const store = require("store");
 
-export const getOptions = (options: any) => store.get(options);
+const server = "http://localhost:4000";
+// const server = "http://192.168.1.103:4000";
+
+export const getStore = (options: any) => store.get(options);
+export const setStore = (options: any, value: any) => store.set(options, value);
+export const removeStore = (options: any) => store.remove(options);
+
+type TMethod =
+  | "get"
+  | "GET"
+  | "delete"
+  | "DELETE"
+  | "head"
+  | "HEAD"
+  | "options"
+  | "OPTIONS"
+  | "post"
+  | "POST"
+  | "put"
+  | "PUT"
+  | "patch"
+  | "PATCH"
+  | "purge"
+  | "PURGE"
+  | "link"
+  | "LINK"
+  | "unlink"
+  | "UNLINK";
+
+export type TSendRequest = {
+  url?: string;
+  data?: any;
+  params?: any;
+  method?: TMethod;
+};
+
+export interface ICount {
+  box1: number;
+  box2: number;
+  box3: number;
+  box4: number;
+  box5: number;
+}
+
+export const user: IUserData = getStore("user") && JSON.parse(getStore("user"));
+export const count: ICount = getStore("count");
+
+export const sendRequest = async ({
+  url,
+  data,
+  params,
+  method = "get",
+}: TSendRequest) => {
+  try {
+    const res = await Axios({
+      url: `${server}/api${url}`,
+      data,
+      params,
+      method,
+      headers: {
+        "auth-token": user?.token || "",
+      },
+    });
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+    console.log(error.response);
+    toast(
+      error.response?.data.message ||
+        RenderMessage().message.serverـconnectionـerror,
+      { type: "error" }
+    );
+    throw error.response;
+  }
+};
